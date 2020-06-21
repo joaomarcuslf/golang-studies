b3b89d806af7fcdbd861a21d04d816ff88c2533a
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2020-06-21 11:07:20 -0300

    Added first Kata

---
+++ katas/morse-code-kata/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var MORSE_CODE = map[string]string{
+	".-":        "A",
+	"-...":      "B",
+	"-.-.":      "C",
+	"-..":       "D",
+	".":         "E",
+	"..-.":      "F",
+	"--.":       "G",
+	"....":      "H",
+	"..":        "I",
+	".---":      "J",
+	"-.-":       "K",
+	".-..":      "L",
+	"--":        "M",
+	"-.":        "N",
+	"---":       "O",
+	".--.":      "P",
+	"--.-":      "Q",
+	".-.":       "R",
+	"...":       "S",
+	"-":         "T",
+	"..-":       "U",
+	"...-":      "V",
+	".--":       "W",
+	"-..-":      "X",
+	"-.--":      "Y",
+	"--..":      "Z",
+	".-.-":      "Ä",
+	".--.-":     "Á",
+	"..-..":     "É",
+	"--.--":     "Ñ",
+	"---.":      "Ö",
+	"..--":      "Ü",
+	"-----":     "0",
+	".----":     "1",
+	"..---":     "2",
+	"...--":     "3",
+	"....-":     "4",
+	".....":     "5",
+	"-....":     "6",
+	"--...":     "7",
+	"---..":     "8",
+	"----":      "9",
+	".-.-.-":    ".",
+	"--..--":    ",",
+	"---...":    ":",
+	"..--..":    "?",
+	".----.":    "'",
+	"-....-":    "-",
+	"-..-.":     "/",
+	"-.--.-":    "(",
+	".-..-.":    "\"",
+	".--.-.":    "@",
+	"-...-":     "=",
+	"........":  "Error",
+	"...−−−...": "SOS",
+}
+
+func DecodeMorse(morseCode string) string {
+	undecoded := strings.Split(strings.ReplaceAll(morseCode, "  ", " | "), " ")
+
+	var decoded = []string{}
+
+	for _, item := range undecoded {
+		if item == "|" {
+			decoded = append(decoded, " ")
+		} else {
+			decoded = append(decoded, MORSE_CODE[item])
+		}
+	}
+
+	return strings.TrimSpace(strings.Join(decoded, ""))
+}
+
+func main() {
+	fmt.Println(DecodeMorse(".... . -.--   .--- ..- -.. .") == "HEY JUDE")
+	fmt.Println(DecodeMorse("   .... . -.--   .--- ..- -.. .") == "HEY JUDE")
+	fmt.Println(DecodeMorse("...−−−...") == "SOS")
+}
