576f5a137d9be6fc57fd438f1b63e1538022469d
João Marcus Fernandes <53450523+joaomarcuslf@users.noreply.github.com>
2021-09-21 14:04:45 -0300

    Update tortoise-hare-find-duplicates.go

--- tortoise-hare-find-duplicates.go
+++ tortoise-hare-find-duplicates.go
@@ -7,9 +7,11 @@ import (
 func findDuplicates(nums []int) int {
 	slow := 0
 	fast := 1
+	aux1 := 0
+	aux2 := 0
 
 	for {
-		if nums[slow] == nums[fast] {
+		if nums[slow] == nums[fast] && slow != fast {
 			return nums[slow]
 		}
 
@@ -17,16 +19,29 @@ func findDuplicates(nums []int) int {
 		fast += 2
 
 		if fast >= len(nums) {
-			fast = 0
+			aux1 += 1
+			
+			if (aux1) >= len(nums) {
+				aux1 = 0
+			}
+			
+			fast = aux1
 		}
 
 		if slow >= len(nums) {
-			slow = 0
+			aux2 += 1
+			
+			if (aux2) >= len(nums) {
+				aux2 = 0
+			}
+			
+			slow = aux2
 		}
 	}
 }
 
 func main() {
 	fmt.Println(findDuplicates([]int{2, 3, 4, 1, 5, 5}))
-	fmt.Println(findDuplicates([]int{1, 2, 4, 4, 3, 3, 1, 5, 3}))
+	fmt.Println(findDuplicates([]int{2, 4, 3, 3, 1, 5, 3}))
+	fmt.Println(findDuplicates([]int{2, 3, 5, 6, 1, 1}))
 }
